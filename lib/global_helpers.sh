360dc380775d715096b13a1419117b932e2130bb
Ivan Karl <ivan6258@gmail.com>
2014-08-04 08:53:38 -0300

    removes unused helpers

--- lib/global_helpers.sh
+++ lib/global_helpers.sh
@@ -2,11 +2,6 @@ _file_exists() {
     [ -f "$1" -o -h "$1" ]
 }
 
-_set_session_file() {
-    local filename=$1
-    _file_exists $filename && echo "$LOCALS_DIR/$filename" || echo "$GLOBALS_DIR/$filename"
-}
-
 _echo_example_file() {
     local example="${YATSH_ROOT}/examples/$1"
     local default="${YATSH_ROOT}/examples/main"
@@ -22,68 +17,15 @@ _capitalize() {
     # echo "$*" | { dd bs=1 count=1 conv=ucase 2> /dev/null; cat ;}
 }
 
-# check if a given command is builtin
-_is_builtin(){
-    local cmd; cmd=$1
-    for plugin in ${BUILTIN_PLUGINS[*]}; do
-        [ "${cmd}" = "${plugin}" ] && return 0
-    done
-    return 1
-}
 _is_command() {
     local command="$1"; shift
     [ ! -z $(command -v yatsh-"$command") ]
 }
 
-_is_plugin() {
-    # local cmd; cmd=$1
-    # for dir in ${PLUGINS_PATH[*]}; do
-    #     [ -d "$dir/$cmd" ] && echo "$dir/$cmd" && return 0
-    # done
-    return 1
-}
-_is_session() {
-    local session=$1
-    # buscar en todas las carpetas de sessiones
-    # TODO: check for a "find" alternative (mac compatible)
-    for dir in $SESSIONS_DIR/*; do
-        for file in $dir/*; do
-            [ $file = $session ] && return 0
-        done
-    done
-    return 1
-}
-
-_find_plugin() {
-    plugin=$1
-    for plugin_dir in $(_split_path $PLUGINS_PATH); do
-        for dir in $plugin_dir/*; do
-            [ "$plugin" = "$dir" ] && echo "$(abs_dirname $dir)/$plugin" && return 0
-        done
-    done
-    return 1
-}
-
-_run_builtin(){
-    local command=$1; shift
-    yatsh-$command $*
-    # . "$PLUGINS_DIR/$command/init.sh" $*
-}
-_run_plugin(){
-    echo running plugin $*
-    # local command=$1; shift
-    # exec "$PLUGINS_DIR/$command/init.sh" $*
-}
 _split_path(){
     local path=$1
     echo "${path//:/$'\n'}"
 }
-
-# _register_sessions_dir(){
-#     local dir=$1
-#     #register some dir
-# }
-#
 
 escape_slashes(){
     echo $* | sed 's/\//\\\//g'
