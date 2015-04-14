afda0ed528f173686f9d632e3765a37bdf7aebd0
Ivan Karl <ivan6258@gmail.com>
2015-04-13 22:52:58 -0300

    adds some session helpers for options

--- lib/session_helpers.sh
+++ lib/session_helpers.sh
@@ -20,7 +20,7 @@ select_layout() { tmux select-layout   -t $SESSION:"$@"; }
 split()         { tmux split-window    -t $SESSION:"$@"; }
 vsplit()        { tmux split-window -h -t $SESSION:"$@"; }
 hsplit()        { tmux split-window -v -t $SESSION:"$@"; }
-get_window_base_index(){ echo $(tmux show-options -g base-index | cut -d' ' -f2); }
+get_window_base_index() { get_global_option base-index;  }
 #}}}
 
 # Panes: #{{{
@@ -28,6 +28,12 @@ select_pane() { tmux select-pane -t $SESSION:"$@"; }
 send_keys()   { tmux send-keys   -t $SESSION:"$@"; }
 swap()        { tmux swap-pane   -t $SESSION:"$@"; }
 send_line()   { send_keys "$@" "Enter"; }
+get_pane_base_index() { get_window_option pane-base-index; }
+#}}}
+
+# General: #{{{
+get_global_option() { tmux show-window-options -g $* | cut -d' ' -f2; }
+get_window_option() { tmux show-window-options -g $* | cut -d' ' -f2; }
 #}}}
 
 # Non tmux helpers #{{{
